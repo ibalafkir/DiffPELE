408d36c603b6baf9bf080e5361e23d15bf12931d
Ismael <ibalafkir@gmail.com>
2025-01-21 10:51:22 +0100

    feat(fastrelax): Finish FastRelax protocol module

--- src/pipeline/fastrelax.py
+++ src/pipeline/fastrelax.py
@@ -5,6 +5,7 @@ import os
 import sys
 from pyrosetta import *
 from pyrosetta.toolbox import *
+import textwrap
 
 
 logger = LoggerFactory.get_logger(__name__, "INFO")
@@ -17,12 +18,30 @@ def fast_relax_protocol(
     relax_protocol
     ):
     
+    """
+    Run and definea FastRelax protocol to a given pose.
+    
+    Parameters:
+    pdb_file : str
+        Path to the PDB file to relax.
+    scorefxn : pyrosetta.ScoreFunction
+        Score function to evaluate the pose.
+    relax_protocol : pyrosetta.protocols.relax.FastRelax
+        FastRelax protocol to apply to the pose.
+    
+    Returns:
+    e_0 : float
+        Initial energy of the pose.
+    e_relaxed : float
+        Energy of the pose after relaxation.
+    """
+    
     # Initial pose
     pose = pose_from_pdb(pdb_file)
     e_0 = scorefxn(pose)
     
     # Side-chain or side-chain and backbone relaxation
-    movemap = pyrosetta.rosetta.core.kinematics.MoveMap() # Remove if you want to relax the backbone
+    #movemap = pyrosetta.rosetta.core.kinematics.MoveMap() # Remove if you want to relax the backbone
     #movemap.set_bb(False)  # do not allow backbone movements. Remove if you want to relax the backbone
     #movemap.set_chi(True)  # allow side chain movements. Remove if you want to relax the backbone
     #movemap.set_jump(False)  # disable rigid-body movements. Remove if you want to relax the backbone
@@ -42,6 +61,16 @@ def fast_relax_protocol(
 def run_fast_relax(
     pdb_file
 ):
+    """
+    Run FastRelax protocol to a given PDB file.
+    
+    Parameters:
+    pdb_file : str
+        Path to the PDB file to relax.
+        
+    Returns:
+        relax_report.tsv : file with the energies of the relaxed poses.
+    """
     
     # Initialize PyRosetta
     init('-relax:default_repeats 2')
@@ -67,10 +96,32 @@ def run_fast_relax(
 
 
 def make_fastrelax_runner(
-    fast_relax_script,
+    path_to_fastrelax_script,
     pdbs_dir
 ):
-    content = """
+    """
+    Generate a bash script to run FastRelax protocol in parallel over multiple PDB files.
+    
+    Parameters:
+    path_to_fastrelax_script : str
+        Path to the script that contains the FastRelax protocol.
+    pdbs_dir : str
+        Path to the directory with PDB files to relax.
+    
+    Returns:
+        fastrelax.sh : bash script to run FastRelax protocol in parallel.
+    
+    """
+    
+    # Get PDBs to relax
+    items = os.listdir(pdbs_dir)
+    pdbs = [pdb for pdb in items if pdb.endswith('.pdb')]
+    
+    # Write string variable with greasy relaxjob info
+    relax_job_greasy = "relaxjob.txt\n"+"\n".join([f"python {path_to_fastrelax_script} {os.path.join(pdbs_dir, pdb)}" for pdb in pdbs])
+    relax_job_greasy = textwrap.indent(relax_job_greasy, '    ')
+    
+    content = f"""
     #!/bin/bash
     #SBATCH --job-name=relax
     #SBATCH --output=logs/%x_%j.out
@@ -81,22 +132,26 @@ def make_fastrelax_runner(
     #SBATCH --account=bsc72 
     #SBATCH --qos=gp_bscls
 
-
     module load greasy
     ml anaconda
     source activate /gpfs/projects/bsc72/conda_envs/pyrosetta
 
-    cat <<EOL > relaxjob.txt
-    python <path/to/fast_relax_script> <iterate_over_pdbs_in_pdb_dir>
+    cat <<EOL > __COMMANDS__
     EOL
 
     greasy relaxjob.txt
     rm relaxjob.txt
     """
     
+    # Adjustments
+    content = content.replace('__NPDBS__', str(len(pdbs)))
+    content = content.replace('__COMMANDS__', relax_job_greasy)
+        
+    # Remove indentation
+    content = textwrap.dedent(content)
+    content = content.lstrip("\n")
     
-if __name__ == '__main__':
-
-    # Parse arguments
-    pdb_file = sys.argv[1]
-    run_fast_relax(pdb_file)
+    # Write content
+    output_path = os.path.join(pdbs_dir, 'fastrelax.sh')
+    with open(output_path, "w") as file:
+        file.write(content)
