9e55c4883998c8fed374d9b3e124fd73c8fe0654
Ismael <ibalafkir@gmail.com>
2025-02-02 20:41:04 +0100

    fix(fastrelax): Correct minor

--- src/pipeline/fastrelax.py
+++ src/pipeline/fastrelax.py
@@ -140,7 +140,7 @@ def make_fastrelax_runner(
     EOL
 
     greasy relaxjob.txt
-    rm relaxjob.txt
+    rm relaxjob.txt    
     """
     
     # Adjustments
@@ -153,6 +153,6 @@ def make_fastrelax_runner(
     content = content.lstrip("\n")
     
     # Write content
-    output_path = os.path.join(os.path.abspath(pdbs_dir), 'fastrelax.sh')
+    output_path = os.path.join(os.path.dirname(pdbs_dir), 'fastrelax.sh')
     with open(output_path, "w") as file:
         file.write(content)
